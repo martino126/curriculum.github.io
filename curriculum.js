b7eacda62990c26e879902c2b861741ef7fceec6
martino126 <126742048+martino126@users.noreply.github.com>
2023-03-08 23:50:53 -0300

    inicio commit

---
+++ curriculum.js
@@ -0,0 +1,5 @@
+
+  
+  function inf() {
+  document.write(" .Conocimientos de redes LAN y WAN - Resolución de problemas - Trabajo bajo presión - Bases de datos Oracle - Bases de datos SQLServer - Administración de la nube")
+ }
